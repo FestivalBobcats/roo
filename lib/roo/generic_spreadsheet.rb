c40f02b523b00c1f809a3bf0c7c90f05e44c1017
Ben Hundley <ben.hundley@gmail.com>
2011-04-11 09:46:25 -0500

    must specifiy :clean => false to avoid converting floats (10.0) to integers (10)

--- lib/roo/generic_spreadsheet.rb
+++ lib/roo/generic_spreadsheet.rb
@@ -404,11 +404,12 @@ class GenericSpreadsheet
     # odd unicode characters and white spaces around columns
 
     def each(options={})
-      if options[:clean]
-        options.delete(:clean)
+      unless options[:clean] == false
         @cleaned ||= {}
         @cleaned[@default_sheet] || clean_sheet
       end
+      
+      options.delete(:clean)
 
       if options.empty?
         @header_line = 1
